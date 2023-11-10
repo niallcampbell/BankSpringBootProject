b95adeedc56bea1de12ff30aad4a58da69f4796e
Niall Campbell <campbellniall@gmail.com>
2023-11-10 16:17:47 +0000

    Added OpenApi spec and basic implementation

---
+++ src/main/java/com/springbootproject/bank/controller/BankAPIController.java
@@ -0,0 +1,15 @@
+package com.springbootproject.bank.controller;
+
+import com.springbootproject.bank.api.BankApi;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+@RequiredArgsConstructor
+public class BankAPIController implements BankApi {
+
+
+
+}
---
+++ src/main/java/com/springbootproject/bank/entities/Customer.java
@@ -0,0 +1,19 @@
+package com.springbootproject.bank.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Customer {
+
+    private Integer id;
+
+    private String Name;
+
+    private Integer age;
+}
